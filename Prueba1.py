6fea094ee50f09991825cfdfd72101cd4b86a318
lopoloalex <lopoloalex@gmail.com>
2018-01-21 17:49:04 +0100

    Configuracion servidores 1.6

--- Prueba1.py
+++ Prueba1.py
@@ -20,24 +20,19 @@ print(" ---- FIN INSTALACION NODEJS ----")
 print(" ---- CLONACION DEL CRM ----")
 os.system("git clone https://github.com/CORE-UPM/CRM_2017.git")
 os.chdir("CRM_2017")
-
-os.system("export DATABASE_URL=postgres://crm:xxxx@10.1.4.31:5432/crm")
 print(" ---- INICIO INSTALACION NPM ----")
 os.system("npm install")
 os.system("npm install forever")
+
 print(" ---- FIN INSTALACION NPM ----")
-
+os.system("export DATABASE_URL=postgres://crm:xxxx@10.1.4.31:5432/crm")
 os.system("export CLOUDINARY_SUBFOLDER=/mnt/nas")
-
-
-# SOLO EN S1
-if socket.gethostname() == "s1":
-	print(" ---- MIGRACION DE LA BASE DE DATOS ----")
-	os.system("npm run-script migrate_local")
-	os.system("npm run-script seed_local")
+print(" ---- MIGRACION DE LA BASE DE DATOS ----")
+os.system("npm run-script migrate_local")
+os.system("npm run-script seed_local")
 
 os.system("sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j REDIRECT --to-port 3000")
-os.system("export DATABASE_URL=postgres://crm:xxxx@10.1.4.31:5432/crm && ./node_modules/forever/bin/forever start ./bin/www")
+os.system("./node_modules/forever/bin/forever start ./bin/www")
 
 
 
