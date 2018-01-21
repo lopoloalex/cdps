b17769a1d7df6df8082eb8feb747d8fde7083ba2
lopoloalex <lopoloalex@gmail.com>
2018-01-21 17:08:10 +0100

    Configuracion servidores 1.3

--- crm_install.py
+++ crm_install.py
@@ -1,25 +1,33 @@
+
+
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
 import sys
 import os
 import socket
 
 print(" ---- ACTUALIZANDO REPOSITORIOS ----")
 os.system("apt update")
+
 print(" ---- DESCARGANDO DEPENDENCIAS DE NODEJS ----")
 os.system("curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -")
+
 print(" ---- INSTALANDO NODEJS ----")
 os.system("apt-get install nodejs")
 print(" ---- FIN INSTALACION NODEJS ----")
 
 print(" ---- CLONACION DEL CRM ----")
 os.system("git clone https://github.com/CORE-UPM/CRM_2017.git")
-
 os.chdir("CRM_2017")
+
+os.system("export DATABASE_URL=postgres://crm:xxxx@10.1.4.31:5432/crm")
+
 print(" ---- INICIO INSTALACION NPM ----")
 os.system("npm install")
 os.system("npm install forever")
 print(" ---- FIN INSTALACION NPM ----")
 
-os.system("export DATABASE_URL=postgres://crm:xxxx@10.1.4.31:5432/crm")
 os.system("export CLOUDINARY_SUBFOLDER=/mnt/nas")
 
 # SOLO EN S1
